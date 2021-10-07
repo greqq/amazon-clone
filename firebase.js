e6c7076c98c700311c2824c84a2a1d699bd6ebce
Luka Masanovic <lukamasanovic@gmail.com>
2021-10-07 19:27:30 +0200

    added auth, redux

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDC7byzqm75G6uhg07_aPtMyDAmxrxZhPM",
+    authDomain: "amzn-clone-9643c.firebaseapp.com",
+    projectId: "amzn-clone-9643c",
+    storageBucket: "amzn-clone-9643c.appspot.com",
+    messagingSenderId: "905501669175",
+    appId: "1:905501669175:web:f63892dc6ebb4d2a844cb2"
+  };
